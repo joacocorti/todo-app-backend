d4563721f43b1afd347b1404a1ec1e305732b8c5
Joaquin Corti <joacocorti@gmail.com>
2023-08-15 20:42:19 -0300

    first commit backend

---
+++ models/ToDoModel.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const todoSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("ToDo", todoSchema);
